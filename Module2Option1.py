fd58ac22f07508ea53dfb2d2e87641835f4ee59a
David M Vermillion <67343577+davidmvermillion@users.noreply.github.com>
2023-11-24 23:14:16 -0700

    New Python file

---
+++ Module2Option1.py
@@ -0,0 +1,10 @@
+# The Python script should have the name of the diagram as input and should output 
+# the names of the stakeholders and the number of communication pathways in your diagram.
+
+# Attempted to import, but UMLet UXF file isn't reading correctly
+# # Set up environment
+# import uxf
+# # a = uxf.load('Module2Option1.uxf')
+
+uxf = input('Name the Diagram and Press Enter\n')
+print('\n', uxf, '\nStakeholders: Amber\nBrad\nNumber of Communication Pathways: 2')
