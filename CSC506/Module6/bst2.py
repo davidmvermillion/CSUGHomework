168864b075d922e0b1182f4ebff9a4a19027fc16
David M Vermillion <67343577+davidmvermillion@users.noreply.github.com>
2024-02-24 16:53:23 -0700

    Insert method works

--- CSC506/Module6/bst2.py
+++ CSC506/Module6/bst2.py
@@ -33,7 +33,8 @@ class Tree(object):
                 curr.right = Node(nums[i])
                 q.append(curr.right)
                 i += 1
-        return root
+        return root # Unclear whether or not this meets the intent of the assignment referencing
+                    # returning the "level-1 root node"
 
     def printTree(root):
         if not root:
@@ -42,10 +43,112 @@ class Tree(object):
         print(root.val, end=" ")
         printTree(root.right)
 
-# Second definition required for Tree class to recognize existence of function
+    # https://www.geeksforgeeks.org/find-the-maximum-depth-or-height-of-a-tree/
+    def maxDepth(node):
+        if node is None:
+            return 0
+        else:
+            lDepth = maxDepth(node.left)
+            rDepth = maxDepth(node.right)
+            if (lDepth > rDepth):
+                return lDepth+1
+            else:
+                return rDepth+1
+            
+    # https://www.geeksforgeeks.org/insertion-in-binary-search-tree/
+    def insert(root, key):
+        if root is None:
+            return Node(key)
+        else:
+            if root.val == key:
+                return root
+            elif root.val < key:
+                root.right = insert(root.right, key)
+            else:
+                root.left = insert(root.left, key)
+        return root
+
+# Second definition required for Tree class to recognize existence of following functions
 def printTree(root):
     if not root:
         return
     printTree(root.left)
     print(root.val, end=" ")
     printTree(root.right)
+
+def maxDepth(node):
+    if node is None:
+        return 0
+    else:
+        lDepth = maxDepth(node.left)
+        rDepth = maxDepth(node.right)
+        if (lDepth > rDepth):
+            return lDepth+1
+        else:
+            return rDepth+1
+
+# https://www.geeksforgeeks.org/insertion-in-binary-search-tree/
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val == key:
+            return root
+        elif root.val < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+
+
+# https://csuglobal.instructure.com/courses/87885/external_tools/51766
+def delete(self, key):
+    parent = None
+    current_node = self.root
+    
+    # Search for the Node.
+    while current_node is not None:
+    
+        # Check if current_node has a matching key.
+        if current_node.key == key: 
+            if current_node.left is None and current_node.right is None:   # Case 1
+                if parent is None: # Node is root
+                    self.root = None
+                elif parent.left is current_node: 
+                    parent.left = None
+                else:
+                    parent.right = None
+                return  # Node found and removed
+            elif current_node.left is not None and current_node.right is None:  # Case 2
+                if parent is None: # Node is root
+                    self.root = current_node.left
+                elif parent.left is current_node: 
+                    parent.left = current_node.left
+                else:
+                    parent.right = current_node.left
+                return  # Node found and removed
+            elif current_node.left is None and current_node.right is not None:  # Case 2
+                if parent is None: # Node is root
+                    self.root = current_node.right
+                elif parent.left is current_node:
+                    parent.left = current_node.right
+                else:
+                    parent.right = current_node.right
+                return  # Node found and removed
+            else:                                    # Case 3
+                # Find successor (leftmost child of right subtree)
+                successor = current_node.right
+                while successor.left is not None:
+                    successor = successor.left
+                current_node.key = successor.key      # Copy successor to current Node
+                parent = current_node
+                current_node = current_node.right     # Remove successor from right subtree
+                key = parent.key                      # Loop continues with new key
+        elif current_node.key < key: # Search right
+            parent = current_node
+            current_node = current_node.right
+        else:                        # Search left
+            parent = current_node
+            current_node = current_node.left
+            
+    return # Node not found
