c3ec54e67ed86e869ca28a0ed31abe903916c40e
David M Vermillion <67343577+davidmvermillion@users.noreply.github.com>
2024-07-17 21:54:06 -0600

    Module 1

---
+++ CSC515/Module1/opencv1.py
@@ -0,0 +1,18 @@
+# Import packages
+import cv2
+from os import chdir
+from os.path import abspath, dirname
+
+# Force script execution directory to current path
+# https://stackoverflow.com/a/69556612/13801562
+chdir(dirname(abspath(__file__)))
+
+# Import image (renamed from downloaded name of 'shutterstock93075775--250.jpg' to be more descriptive)
+image = cv2.imread('brain.jpg')
+
+# Show image and name window
+cv2.imshow('Brain Image', image)
+
+# https://www.geeksforgeeks.org/python-opencv-cv2-imshow-method/
+# This step is required to prevent auto-closing/crashing upon run. Output can be exited by pressing any key.
+cv2.waitKey(0)
