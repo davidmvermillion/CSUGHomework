0e4a7c8a916eee5c8553a7433e993059054e0450
David M Vermillion <67343577+davidmvermillion@users.noreply.github.com>
2024-07-18 20:37:14 -0600

    Module1 Script Complete

--- CSC515/Module1/opencv1.py
+++ CSC515/Module1/opencv1.py
@@ -1,7 +1,7 @@
 # Import packages
 import cv2
 from os import chdir
-from os.path import abspath, dirname
+from os.path import abspath, dirname, join
 
 # Force script execution directory to current path
 # https://stackoverflow.com/a/69556612/13801562
@@ -9,6 +9,12 @@ chdir(dirname(abspath(__file__)))
 
 # Import image (renamed from downloaded name of 'shutterstock93075775--250.jpg' to be more descriptive)
 image = cv2.imread('brain.jpg')
+
+# Put image in desktop folder. This path is machine-specific and is not portable.
+# I don't like putting things on the desktop. I also don't like writing this as an absolute path.
+# https://stackoverflow.com/a/41587740/13801562
+newimagepath = '/Users/davidmvermillion/Desktop/PlzNoMoreDesktop'
+cv2.imwrite(join(newimagepath, 'brain.jpg'), image)
 
 # Show image and name window
 cv2.imshow('Brain Image', image)
