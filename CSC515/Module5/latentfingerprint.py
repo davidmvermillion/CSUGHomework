f1790f9d0bdcad2d5f43e7b0174161c30bebaa9b
David M Vermillion <67343577+davidmvermillion@users.noreply.github.com>
2024-08-17 22:44:40 -0600

    Comparing grayscale vs proper binary

--- CSC515/Module5/latentfingerprint.py
+++ CSC515/Module5/latentfingerprint.py
@@ -16,16 +16,14 @@ image = cv2.imread('fingerprint.jpg')
 kernel = np.ones((3, 3), np.uint8)
 
 # Try various individual operations
-# Binary
-
-
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-(thresh, binary_img) = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-(thresh, binary_img2) = cv2.threshold(gray, 127, 648, cv2.THRESH_BINARY)
-binary_img3 = cv2.adaptiveThreshold(gray, 127, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-binary_img4 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
 # Grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Global Binary
+(thresh, binary_img) = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+# Adaptive Binary
+binary_img2 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+
+# Grayscale Morphological Operations
 # Erosion
 erosion = cv2.erode(gray, kernel, iterations = 1)
 erosion2 = cv2.erode(gray, kernel, iterations = 2)
@@ -37,49 +35,64 @@ opening = cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel)
 # Close
 closing = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel)
 
-images = [binary_img, binary_img2, binary_img3, binary_img4]
-for i in range(4):
-    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+# Binary Morphological Operations
+# Erosion
+erosionb = cv2.erode(binary_img2, kernel, iterations = 1)
+erosion2b = cv2.erode(binary_img2, kernel, iterations = 2)
+# Dilation
+dilationb = cv2.dilate(binary_img2, kernel, iterations = 1)
+dilation2b = cv2.dilate(binary_img2, kernel, iterations = 2)
+# Open
+openingb = cv2.morphologyEx(binary_img2, cv2.MORPH_OPEN, kernel)
+# Close
+closingb = cv2.morphologyEx(binary_img2, cv2.MORPH_CLOSE, kernel)
+
+images = [erosionb, dilationb, openingb, closingb,
+          erosion, dilation, opening, closing]
+for i in range(8):
+    plt.subplot(2,4,i+1),plt.imshow(images[i],'gray')
     plt.xticks([]),plt.yticks([])
+plt.suptitle('Binary Morphology on Top\nGrayscale Morphology on Bottom', fontsize = 25).set_color('#171819')
+plt.tight_layout()
 plt.show()
 
-# # Display results
-# fig, axs = plt.subplots(nrows = 3, ncols = 3)
-# fig.suptitle('Latent Fingerprint Morpholigical\nOperations had Varying Effects', fontsize = 25).set_color('#171819')
+# Display results
+fig, axs = plt.subplots(nrows = 3, ncols = 3)
+fig.suptitle('Latent Fingerprint Morpholigical\nOperations had Varying Effects', fontsize = 25).set_color('#171819')
 
-# # Images
-# axs[0, 0].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-# axs[0, 1].imshow(cv2.cvtColor(gray, cv2.COLOR_BGR2RGB))
-# axs[0, 2].imshow(cv2.cvtColor(binary_img, cv2.COLOR_BGR2RGB))
-# axs[1, 0].imshow(cv2.cvtColor(opening, cv2.COLOR_BGR2RGB))
-# axs[1, 1].imshow(cv2.cvtColor(erosion, cv2.COLOR_BGR2RGB))
-# axs[1, 2].imshow(cv2.cvtColor(dilation, cv2.COLOR_BGR2RGB))
-# axs[2, 0].imshow(cv2.cvtColor(closing, cv2.COLOR_BGR2RGB))
-# axs[2, 1].imshow(cv2.cvtColor(erosion2, cv2.COLOR_BGR2RGB))
-# axs[2, 2].imshow(cv2.cvtColor(dilation2, cv2.COLOR_BGR2RGB))
+# Images
+axs[0, 0].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+axs[0, 1].imshow(cv2.cvtColor(gray, cv2.COLOR_BGR2RGB))
+axs[0, 2].imshow(cv2.cvtColor(binary_img, cv2.COLOR_BGR2RGB))
+axs[1, 0].imshow(cv2.cvtColor(opening, cv2.COLOR_BGR2RGB))
+axs[1, 1].imshow(cv2.cvtColor(erosion, cv2.COLOR_BGR2RGB))
+axs[1, 2].imshow(cv2.cvtColor(dilation, cv2.COLOR_BGR2RGB))
+axs[2, 0].imshow(cv2.cvtColor(closing, cv2.COLOR_BGR2RGB))
+axs[2, 1].imshow(cv2.cvtColor(erosion2, cv2.COLOR_BGR2RGB))
+axs[2, 2].imshow(cv2.cvtColor(dilation2, cv2.COLOR_BGR2RGB))
 
-# # Titles
-# axs[0, 0].set_title('Original')
-# axs[0, 1].set_title('Grayscale')
-# axs[0, 2].set_title('Binary')
-# axs[1, 0].set_title('Opening')
-# axs[1, 1].set_title('Erosion')
-# axs[1, 2].set_title('Dilation')
-# axs[2, 0].set_title('Closing')
-# axs[2, 1].set_title('Erosion x2')
-# axs[2, 2].set_title('Dilation x2')
+# Titles
+axs[0, 0].set_title('Original')
+axs[0, 1].set_title('Grayscale')
+axs[0, 2].set_title('Binary')
+axs[1, 0].set_title('Opening')
+axs[1, 1].set_title('Erosion')
+axs[1, 2].set_title('Dilation')
+axs[2, 0].set_title('Closing')
+axs[2, 1].set_title('Erosion x2')
+axs[2, 2].set_title('Dilation x2')
 
-# # Turn off individual image axes
-# axs[0, 0].axis('off')
-# axs[0, 1].axis('off')
-# axs[0, 2].axis('off')
-# axs[1, 0].axis('off')
-# axs[1, 1].axis('off')
-# axs[1, 2].axis('off')
-# axs[2, 0].axis('off')
-# axs[2, 1].axis('off')
-# axs[2, 2].axis('off')
+# Turn off individual image axes
+axs[0, 0].axis('off')
+axs[0, 1].axis('off')
+axs[0, 2].axis('off')
+axs[1, 0].axis('off')
+axs[1, 1].axis('off')
+axs[1, 2].axis('off')
+axs[2, 0].axis('off')
+axs[2, 1].axis('off')
+axs[2, 2].axis('off')
 
-# # Show results
-# fig.tight_layout()
-# plt.show()
+# Show results
+fig.tight_layout()
+plt.show()
