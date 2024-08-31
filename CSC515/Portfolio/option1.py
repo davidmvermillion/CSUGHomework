08444644ffe1dbc1c71cb5b6b9cabd5747ba7e9f
David M Vermillion <67343577+davidmvermillion@users.noreply.github.com>
2024-08-31 15:46:07 -0600

    List works
    Had to add folder to pull valid files and define gray before calling it

--- CSC515/Portfolio/option1.py
+++ CSC515/Portfolio/option1.py
@@ -9,54 +9,56 @@ import numpy as np
 chdir(dirname(abspath(__file__)))
 
 # Read images
-image1 = cv2.imread('Image2.jpg') # RUS License Plate w/ Shadow
-image2 = cv2.imread('Image3.jpg') # Clear RUS License Plate Lighting
-image3 = cv2.imread('Image5.jpg') # Two European Plates w/ Flat Lighting
-source = [image1, image2, image3] # Clean this up to be correct list
+image1 = cv2.imread('Images/Image2.jpg') # RUS License Plate w/ Shadow
+image2 = cv2.imread('Images/Image3.jpg') # Clear RUS License Plate Lighting
+image3 = cv2.imread('Images/Image5.jpg') # Two European Plates w/ Flat Lighting
+source = [image1, image2, image3]
+gray = []
+
 
 # Convert to Grayscale
-for i in range(len(source)): # Verify images are called correctly
+for i in source: # Verify images are called correctly
     gray[i] = cv2.cvtColor(source[i], cv2.COLOR_BGR2GRAY) # Verify it works without gray being pre-declared
 
-# Load Classifier
-rusPlateFinder16 = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_license_plate_rus_16stages.xml')
-rusPlateReader = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_russian_plate_number.xml')
+# # Load Classifier
+# rusPlateFinder16 = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_license_plate_rus_16stages.xml')
+# rusPlateReader = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_russian_plate_number.xml')
 
 
-# Detect Plates
-for i in gray
-plates = rusPlateFinder16.detectMultiScale(
-    gray[i],
-    scaleFactor = 1.1,
-    minNeighbors = 5,
-    minSize = (40, 40)
-)
+# # Detect Plates
+# for i in gray
+# plates = rusPlateFinder16.detectMultiScale(
+#     gray[i],
+#     scaleFactor = 1.1,
+#     minNeighbors = 5,
+#     minSize = (40, 40)
+# )
 
-# Implement processing if required to make plates horizontal
+# # Implement processing if required to make plates horizontal
 
-# Boundary Boxes around Detected Plates
+# # Boundary Boxes around Detected Plates
 
-for (x, y, w, h) in plates:
-   roi_gray = gray[y: y + h, x: x + w]
-   roi_color = source[y: y + h, x: x + w]
+# for (x, y, w, h) in plates:
+#    roi_gray = gray[y: y + h, x: x + w]
+#    roi_color = source[y: y + h, x: x + w]
    
-   # Detect Plates and name after the philosopher
-   plato = rusPlateReader.detectMultiScale(roi_gray)
+#    # Detect Plates and name after the philosopher
+#    plato = rusPlateReader.detectMultiScale(roi_gray)
    
-   # Create coordinates and parameters
-   plato_x = min(plato[:, 0])
-   plato_y = min(plato[:, 1])
-   plato_h = max(plato[:, 3])
-   plato_w = max(plato[:, 0]) + max(plato[:, 2]) - min(plato[:, 0])
+#    # Create coordinates and parameters
+#    plato_x = min(plato[:, 0])
+#    plato_y = min(plato[:, 1])
+#    plato_h = max(plato[:, 3])
+#    plato_w = max(plato[:, 0]) + max(plato[:, 2]) - min(plato[:, 0])
 
-   # Create rectangle around detected plates
-   cv2.rectangle(
-      roi_color,
-      (plato_x, plato_y),
-      (plato_x + plato_w, plato_y + plato_h),
-      (0, 0, 255),
-      8
-   )
+#    # Create rectangle around detected plates
+#    cv2.rectangle(
+#       roi_color,
+#       (plato_x, plato_y),
+#       (plato_x + plato_w, plato_y + plato_h),
+#       (0, 0, 255),
+#       8
+#    )
 
 # Figure out how to display results once confirmed that they work
 
