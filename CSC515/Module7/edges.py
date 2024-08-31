7e643193d478733e3ce409192a071bee5019be87
David M Vermillion <67343577+davidmvermillion@users.noreply.github.com>
2024-08-30 20:58:47 -0600

    Noisy Image added

--- CSC515/Module7/edges.py
+++ CSC515/Module7/edges.py
@@ -10,7 +10,7 @@ chdir(dirname(abspath(__file__)))
 
 # Create Square and Circle in Background
 # https://medium.com/jungletronics/opencv-image-basics-2e63d973851a
-background = np.ones(shape = (300, 500, 3))
+background = np.ones(shape = (300, 500, 3), dtype = np.uint8)
 cv2.rectangle(background, pt1 = (50, 50), pt2 = (150, 150), color = (0, 0, 227), thickness = cv2.FILLED)
 circle = cv2.circle(background, center = (350, 200), radius = 50, color = (0, 128, 0), thickness = cv2.FILLED)
 # https://stackoverflow.com/a/56186046/13801562
@@ -25,11 +25,23 @@ laplacian = cv2.Laplacian(circle, cv2.CV_8U)
 
 # Measure of performance is amount of outline
 
+# Add noise
+# https://www.askpython.com/python/examples/adding-noise-images-opencv
+def addGaussianNoise(image, mean = 0, std = 25):
+    noise = np.random.normal(mean, std, image.shape).astype(np.uint8)
+    noisy_image = cv2.add(image, noise)
+    return noisy_image
+
+# Noisy version
+noisyversion = addGaussianNoise(background)
+cannyn = cv2.Canny(noisyversion, 110, 400)
+sobeln = cv2.Sobel(noisyversion, ddepth = cv2.CV_8U, dx = 1, dy = 0, ksize = 7)
+laplaciann = cv2.Laplacian(circle, cv2.CV_8U)
 
 # cv2.imshow('Original Image', background)
 # cv2.imshow('Canny', canny)
-cv2.imshow('Laplacian', laplacian)
-cv2.waitKey(0)
+# cv2.imshow('Laplacian', cannyn)
+# cv2.waitKey(0)
 
 # titles = ['Dark to Light\nGrayscale', 'Light to Dark\nGrayscale',
 #           'Dark to Light\nMean', 'Dark to Light\nGaussian',
