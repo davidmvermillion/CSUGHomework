0694a0425affd0b9f1548a23f6283b4c9dc94663
David M Vermillion <67343577+davidmvermillion@users.noreply.github.com>
2024-09-01 12:25:35 -0400

    Color Change
    License Plates detected, but also a ghost one in the second image

--- CSC515/Portfolio/option1.py
+++ CSC515/Portfolio/option1.py
@@ -9,8 +9,6 @@ import numpy as np
 chdir(dirname(abspath(__file__)))
 
 # Read images
-# image1 = cv2.imread('Images/Image2.jpg') # RUS License Plate w/ Shadow
-# image1 = cv2.imread('Images/Image1.jpg') # Hungarian USSR Plate
 image1 = cv2.imread('Images/Image6.jpg') # Russian Taxi at Angle
 image2 = cv2.imread('Images/Image3.jpg') # Clear RUS License Plate Lighting
 image3 = cv2.imread('Images/Image5.jpg') # Two European Plates w/ Flat Lighting
@@ -21,7 +19,7 @@ source = np.array(source, dtype = list)
 # Define placeholders
 gray = np.array([0, 1, 2], dtype = list)
 plates = np.array(list(range(3)), dtype = list)
-plates2 = np.array(list(range(3)), dtype = list)
+# plates2 = np.array(list(range(3)), dtype = list)
 
 # Convert to Grayscale
 for i in range(len(source)):
@@ -32,53 +30,28 @@ rusPlateReader = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_lice
 rusPlateFinder = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_russian_plate_number.xml')
 
 # Detect Plates
-for i in range(len(gray)):
-    plates[i] = rusPlateFinder.detectMultiScale(
-        gray[i],
-        scaleFactor = 1.1,
-        minNeighbors = 5,
-        minSize = (40, 40)
-    )
-
 # https://github.com/kennethleungty/Car-Plate-Detection-OpenCV-TesseractOCR/blob/main/Car%20Plate%20Detection%20with%20OpenCV%20and%20TesseractOCR.ipynb
-def carplate_detect(image):
+def CarplateDetect(image):
     carplate_overlay = image.copy() # Create overlay to display red rectangle of detected car plate
-    carplate_rects = rusPlateFinder.detectMultiScale(carplate_overlay,scaleFactor=1.1, minNeighbors=5) 
+    carplate_rects = rusPlateFinder.detectMultiScale(carplate_overlay,
+                                                     scaleFactor = 1.1,
+                                                     minNeighbors = 5) 
 
-    for x,y,w,h in carplate_rects: 
-        cv2.rectangle(carplate_overlay, (x,y), (x+w,y+h), (255,0,0), 5) 
+    for x, y, w, h in carplate_rects: 
+        cv2.rectangle(carplate_overlay,
+                      (x, y),
+                      (x + w, y + h),
+                      (0, 0, 255),
+                      25) 
         
     return carplate_overlay
 
-for i in range(len(gray)):
-    plates2[i] = carplate_detect(gray[i])
+for i in range(len(source)):
+    plates[i] = CarplateDetect(source[i])
 
 
 # # Implement processing if required to make plates horizontal
 
-# Boundary Boxes around Detected Plates
-
-# for (x, y, w, h) in plates:
-#    roi_gray = gray[y: y + h, x: x + w]
-#    roi_color = source[y: y + h, x: x + w]
-   
-#    # Detect Plates and name after the philosopher
-#    plato = rusPlateReader.detectMultiScale(roi_gray)
-   
-#    # Create coordinates and parameters
-#    plato_x = min(plato[:, 0])
-#    plato_y = min(plato[:, 1])
-#    plato_h = max(plato[:, 3])
-#    plato_w = max(plato[:, 0]) + max(plato[:, 2]) - min(plato[:, 0])
-
-#    # Create rectangle around detected plates
-#    cv2.rectangle(
-#       roi_color,
-#       (plato_x, plato_y),
-#       (plato_x + plato_w, plato_y + plato_h),
-#       (0, 0, 255),
-#       8
-#    )
 
 # Figure out how to display results once confirmed that they work
 
@@ -89,10 +62,11 @@ for i in range(len(gray)):
 #           noisyversion, cannyn, sobeln, laplaciann]
 
 # Convert BGR to RGB
-# cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
+for i in range(len(plates)):
+    plates[i] = cv2.cvtColor(plates[i], cv2.COLOR_BGR2RGB)
 
 for i in range(3):
-    plt.subplot(3, 1, i + 1), plt.imshow(plates2[i], 'gray')
+    plt.subplot(3, 1, i + 1), plt.imshow(plates[i], 'gray')
     plt.xticks([]), plt.yticks([])
     # plt.title(titles[i])
     plt.tight_layout()
