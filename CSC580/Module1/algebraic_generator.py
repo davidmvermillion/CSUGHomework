c24b6d2f790d8e2c1c5beb5c16601b649cdfe989
David M Vermillion <67343577+davidmvermillion@users.noreply.github.com>
2024-09-12 20:55:28 -0600

    First load

--- CSC580/Module1/algebraic_generator.py
+++ CSC580/Module1/algebraic_generator.py
@@ -1,8 +1,6 @@
 # Initial code from Module 1 Critical Thinking Page
 from random import randint
 from sklearn.linear_model import LinearRegression
-from skl2onnx import to_onnx
-from numpy import int_
 from pickle import dump
 from os import chdir
 from os.path import abspath, dirname
@@ -34,13 +32,6 @@ X_test = [[10, 20, 30]]
 outcome = predictor.predict(X = X_test)
 coefficients = predictor.coef_
 print('Outcome : {}\nCoefficients : {}'.format(outcome, coefficients))
-
-# Exporting and importing models using ONNX
-# https://scikit-learn.org/stable/model_persistence.html#onnx
-# https://onnx.ai/sklearn-onnx/api_summary.html
-# onx = to_onnx(predictor, train_input, initial_types = int_, target_opset = 12)
-# with open("filename.onnx", "wb") as f:
-#     f.write(onx.SerializeToString())
 
 # Exporting and importing models using pickle
 # Note security issues suggest this should not be used for deployed models
--- CSC580/Module1/user_algebra.py
+++ CSC580/Module1/user_algebra.py
@@ -1,1 +1,15 @@
 from sklearn.linear_model import LinearRegression
+from pickle import load
+from os import chdir
+from os.path import abspath, dirname
+
+# Force script execution directory to current path
+chdir(dirname(abspath(__file__)))
+
+with open("algebra.pkl", "rb") as f:
+    predictor = load(f)
+
+X_test = [[10, 20, 30]]
+outcome = predictor.predict(X = X_test)
+coefficients = predictor.coef_
+print('Outcome : {}\nCoefficients : {}'.format(outcome, coefficients))
